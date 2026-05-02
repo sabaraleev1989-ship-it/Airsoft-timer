0d8d96c65b332d810dc9dd4c675a61724be2fc67
sabaraleev1989-ship-it <sabaraleev1989@gmail.com>
2026-05-02 19:45:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,26 @@
 const CACHE_NAME = 'strikeball-v2';
 const urlsToCache = [
   'index.html',
-  'manifest.json'
+  'manifest.json',
+  'logo3.png'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // сразу активировать новый SW
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener('activate', event => {
+  // Удаляем старые кэши
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  return self.clients.claim(); // начинаем контролировать все вкладки
 });
 
 self.addEventListener('fetch', event => {
