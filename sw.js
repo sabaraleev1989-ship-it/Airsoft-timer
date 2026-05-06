5708fd2f8aa0d576b21273f4994ef9efc931f533
sabaraleev1989-ship-it <sabaraleev1989@gmail.com>
2026-05-06 11:20:35 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,46 @@
-const CACHE_NAME = 'strikeball-v7';
+const CACHE_NAME = 'scorptimer-v1';
 const urlsToCache = [
-    'index.html',
-    'manifest.json',
-    'logo3.png',
-    'siren.mp3',
-    'siren1.mp3',
-    'siren2.mp3',
-    'free_ids.txt'
+  'index.html',
+  'manifest.json',
+  'logo3.png',
+  'logo6.png',
+  'siren.mp3',
+  'siren1.mp3',
+  'siren2.mp3',
+  'siren3.mp3',
+  'start.mp3',
+  'kras.mp3',
+  'sinie.mp3',
+  'zelen.mp3',
+  'zeltie.mp3',
+  'bel.mp3'
 ];
 
-self.addEventListener('install', event => {
-    self.skipWaiting();
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-        ))
-    );
-    return self.clients.claim();
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(key) { return key !== CACHE_NAME; })
+            .map(function(key) { return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
